494a524ea4649ff870da7cae5f3f88f503ccb2dc
alex <camward@yandex.ru>
2016-12-13 22:10:29 +0400

    Метатеги

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -15,13 +15,16 @@ use Yii;
 class CategoryController extends AppController
 {
     public function actionIndex(){
+        $this->setMeta('E-SHOPPER');
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
         return $this->render("index", compact('hits'));
     }
 
     public function actionView($id){
+        $category = Category::findOne($id);
+        $this->setMeta('E-SHOPPER | ' . $category->name, $category->keywords, $category->description);
         $id = Yii::$app->request->get('id');
         $products = Product::find()->where(['category_id' => $id])->all();
-        return $this->render('view', compact('products'));
+        return $this->render('view', compact('products', 'category'));
     }
 }
