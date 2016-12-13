6c48655e674c34729b6c40b57f11b5dd40a8d087
alex <camward@yandex.ru>
2016-12-13 21:14:41 +0400

    Вывод популярных товаров

---
+++ controllers/AppController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 13.12.16
+ * Time: 20:09
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class AppController extends Controller
+{
+
+}
